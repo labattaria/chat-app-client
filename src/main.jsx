1f755fad1e03b9905f1168cd66cdd5128572faee
labattaria <107855108+labattaria@users.noreply.github.com>
2025-06-29 05:22:56 +0300

    build the app

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import 'bulma/css/bulma.css';
-import './style.css';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
